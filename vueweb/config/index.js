a484a0727d6c8ad78036dc7fd025fbe8cf915b2c
paipaipaipaipai <243677721@qq.com>
2020-01-20 14:01:28 +0800

    test

--- vueweb/config/index.js
+++ vueweb/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '119.119.118.49', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
